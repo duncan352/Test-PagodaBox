69635f4f6a5bf0642e25c030f460fb726f0c4000
Chris <cduncan@delorum.com>
2011-02-04 13:09:47 -0700

    Added fix

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 // we connect to localhost and socket e.g. /tmp/mysql.sock
 
 // variant 2: with localhost
-$link = mysql_connect('127.0.0.1:/tmp/mysql/nannie.sock', 'john', '9fHvrbYG');
+$link = mysql_connect('localhost:/tmp/mysql/nannie.sock', 'john', '9fHvrbYG');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
 echo 'Connected successfully';
-
-
 
 mysql_close($link);
 ?>
