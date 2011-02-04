308dc931491547d29322506300a464e0e480a577
Chris <cduncan@delorum.com>
2011-02-04 12:56:27 -0700

    Added coolness

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-phpinfo();
+// we connect to localhost and socket e.g. /tmp/mysql.sock
+
+// variant 2: with localhost
+$link = mysql_connect('localhost:/tmp/mysql/nannie.sock', 'john', '9fHvrbYG');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
 ?>
